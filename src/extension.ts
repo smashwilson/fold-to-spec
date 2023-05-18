a90abf5b148358a853c2ce5188e7fb72028bc89a
Ash Wilson <smashwilson@github.com>
2023-05-18 12:51:22 -0400

    The rest of the owl

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,19 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as Parser from 'web-tree-sitter';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const handler = new RSpecHandler()
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "fold-to-spec" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('fold-to-spec.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from fold-to-spec!');
+	let disposable = vscode.commands.registerCommand('fold-to-spec.fold', async () => {
+		const editor = vscode.window.activeTextEditor
+		if (!editor) {
+			vscode.window.showErrorMessage('No active editor');
+			return
+		}
+		const contents = editor.document.getText()
+		const foldPoints = await handler.foldRowsFor(contents)
+		vscode.commands.executeCommand('editor.fold', { selectionLines: foldPoints })
 	});
 
 	context.subscriptions.push(disposable);
@@ -24,3 +21,50 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+const FOLDED_METHOD_NAMES = new Set([
+	'it', 'fit', 'xit', 'before', 'after', 'around', 'let', 'let!',
+])
+
+class RSpecHandler {
+	private parser: Parser | null = null
+
+	async foldRowsFor(source: string): Promise<number[]> {
+		const parser = await this.getInitializedParser()
+		const tree = parser.parse(source)
+
+		let foldPoints: number[] = []
+		for (const callNode of tree.rootNode.descendantsOfType('call')) {
+			if (this.shouldFoldCall(callNode)) {
+				foldPoints.push(callNode.startPosition.row)
+			}
+		}
+		for (const methodNode of tree.rootNode.descendantsOfType('method')) {
+			foldPoints.push(methodNode.startPosition.row)
+		}
+		return foldPoints
+	}
+
+	private async getInitializedParser(): Promise<Parser> {
+		if (this.parser) {
+			return this.parser
+		}
+
+		await Parser.init()
+		this.parser = new Parser()
+		const rubyWasmPath = path.join(__dirname, '../parsers/tree-sitter-ruby.wasm')
+		const RubyLanguage = await Parser.Language.load(rubyWasmPath)
+		this.parser.setLanguage(RubyLanguage)
+
+		return this.parser
+	}
+
+	private shouldFoldCall(callNode: Parser.SyntaxNode): boolean {
+		const methodName = callNode.children.find(node => node.type === 'identifier')
+		if (!methodName) {
+			return false
+		}
+
+		return FOLDED_METHOD_NAMES.has(methodName.text)
+	}
+}
